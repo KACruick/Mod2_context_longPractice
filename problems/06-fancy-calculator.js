5f9f13c519780b0f62b20adf71105372fdd2e8d1
Imani Roberts <imaniroberts.dev@gmail.com>
2024-04-30 20:44:52 -0400

    done

--- problems/06-fancy-calculator.js
+++ problems/06-fancy-calculator.js
@@ -1,10 +1,26 @@
-// Your code here 
+let Calculator = require("./02-calculator");
+
+class FancyCalculator extends Calculator {
+  constructor() {
+    super();
+    this.total = 0;
+  }
+  setTotal(newTotal) {
+    return (this.total = newTotal);
+  }
+  modulo(num) {
+    return (this.total %= num);
+  }
+  squared() {
+    return (this.total *= this.total);
+  }
+}
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
 try {
-	module.exports = FancyCalculator;
+  module.exports = FancyCalculator;
 } catch {
-	module.exports = null;
+  module.exports = null;
 }
